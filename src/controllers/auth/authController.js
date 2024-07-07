f3b8a99d171ad7c13daf824d5c87dac2dbf5f089
bhuws <ayeitsbhuws@gmail.com>
2024-07-07 20:02:46 +0600

    --login-done

--- src/controllers/auth/authController.js
+++ src/controllers/auth/authController.js
@@ -27,4 +27,26 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { register };
+
+const login = async (req, res ) => {
+  const {email, password} = req.body;
+  try{
+    const user = await User.findOne({email});
+    if(!user){
+      return res.status(404).json({message: "User not found"})
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+      return res.status(401).json({message: "Invalid credentials"})
+    }
+    const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: "1d"});
+    res.status(200).json({message: "Login successful", token});
+
+  } catch(err){
+    console.error(err)
+    return res.status(201).json({message: "Internal Server Error"})
+  }
+}
+
+module.exports = { register, login };
