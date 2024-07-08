d754d274dbf916ff34d464387df76049ce2e6bb5
bhuws <ayeitsbhuws@gmail.com>
2024-07-08 22:03:28 +0600

    --update-user-controller

--- src/controllers/user/userController.js
+++ src/controllers/user/userController.js
@@ -4,4 +4,8 @@ const profile = async (req, res) => {
     return res.json({message: "User profile after auth middleware."})
 }
 
+const resetPassword = async (req, res) => {
+    return res.json({message: "Password reset."})
+}
+
 module.exports = { profile }
