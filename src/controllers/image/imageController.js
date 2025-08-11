530af33fc08ea989ee6eb72999bb9a488d6992bf
ngawang-zangphel <ngawangzangphel360@gmail.com>
2025-08-11 18:52:16 +0600

    cloudinary config

--- src/controllers/image/imageController.js
+++ src/controllers/image/imageController.js
@@ -41,6 +41,25 @@ const uploadImage = async(req, res) => {
     }
 }
 
+const fetchALlImagesController = async (req, res) => {
+    try {
+        const images = await Image.find();
+        if (images) {
+            res.status(200).json({
+                success: true,
+                data: images
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: 'Something went wrong! Please try again'
+        })
+    }
+}
+
 module.exports = {
-    uploadImage
+    uploadImage,
+    fetchALlImagesController
 }
--- src/routes/image/image.js
+++ src/routes/image/image.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const uploadMiddleware = require('./../../middlewares/upload-middleware');
-const { uploadImage: uploadImageController } = require('../../controllers/image/imageController');
+const { uploadImage: uploadImageController, fetchALlImagesController } = require('../../controllers/image/imageController');
 const router = express.Router();
 const { verifyUser } = require("../../middlewares/verifyuser");
 
@@ -8,6 +8,6 @@ const { verifyUser } = require("../../middlewares/verifyuser");
 //upload the image
 //use multer middleware:
 router.post('/upload', uploadMiddleware.single('image'), uploadImageController);
-
+router.get('/', fetchALlImagesController);
 //to get all the images
 module.exports = router;
